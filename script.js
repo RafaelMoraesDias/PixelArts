ae24e600ed06dbf58da6461ffc67fa88ec679459
Rafael Moraes Dias <rafasjbvb@yahoo.com.br>
2021-12-09 18:33:25 -0300

    requisito 6

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-function criaQuadro(){
-    let quadro = document.getElementsByClassName("pixel")
-    let contador = 0;
-    for ( let i = 0; i<5; i+=1){
-        for ( let x = 0; x<5; x+=1){
-           if(i === x){
-               contador += quadro;
-           }
+window.onload = blackSelector;
 
-        }
-    }
-   let bloco = document.createElement("classe");
-   bloco.innerHTML= "quadrados";
-   quadro.appendChield (bloco);
+function blackSelector (){
+    blackcolor = document.getElementById("black");
+}
 
-    
- }
-criaQuadro();
