f6abb688ab3ec2e67ef5466a4934ba19d636b2de
Rafael Moraes Dias <rafasjbvb@yahoo.com.br>
2021-12-11 00:58:41 -0300

    Exercicio 7

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-window.onload = blackSelector;
+window.onload = blackColor;
 
-function blackSelector (){
-    blackcolor = document.getElementById("black");
+function blackColor (){
+    let preto = document.querySelector("#black")
+
 }
+
+const blackButton = document.getElementById('black');
+const greenButton = document.getElementById('green');
+const yellowButton = document.getElementById('yellow');
+const redButton = document.getElementById('red');
+
+blackButton.addEventListener('click', changeClass);
+greenButton.addEventListener('click', changeClass);
+yellowButton.addEventListener('click', changeClass);
+redButton.addEventListener('click', changeClass);
+
+function changeClass(event){
+    let corSelecionada = document.getElementsByClassName("selected")[0];
+    corSelecionada.classList.remove("selected");
+    event.target.classList.add("selected");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
