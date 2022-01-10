9367ab8b2c09c4bfd76c1c75f34df91c395035ce
Rafael Moraes Dias <rafasjbvb@yahoo.com.br>
2022-01-10 14:00:05 -0300

    todos requisitos ok

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-window.onload = blackColor;
-
+window.onload = function(){
+//blackColor();
+randonColor();
 geraQuadro(5);
+}
+
+
 
 
 
@@ -27,7 +31,7 @@ function changeClass(event){
 }
 
  function mudaCor (event){
-    let pincel = document.querySelector('.selected').id;
+    let pincel = document.querySelector('.selected').style.backgroundColor;
     event.target.style.backgroundColor = pincel;  
 
  }
@@ -80,3 +84,24 @@ function verificaQuadro(){
         }
         geraQuadro(input);
 }   
+
+
+//referência no site:
+//https://css-tricks.com/snippets/javascript/random-hex-color/
+
+let cores = document.getElementsByClassName("color");
+function randonColor(){
+
+    for (let index of cores){
+   
+   
+        cores[0].style.backgroundColor = "black";
+        cores[1].style.backgroundColor = `rgb(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)})`;
+        cores[2].style.backgroundColor = `rgb(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)})`;
+        cores[3].style.backgroundColor = `rgb(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)})`;
+       
+ }
+}
+  
+ 
+
